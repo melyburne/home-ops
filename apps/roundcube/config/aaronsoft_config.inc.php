edb3dd5d06ca508cf229a092195d4d5af69f198e
Aaron Hermann <a.hermann.1@gmx.de>
2026-03-03 12:33:13 +0100

    Fixed branding in roundcube

--- apps/roundcube/config/aaronsoft_config.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-$config['product_name'] = 'Aaronsoft Webmail';
-$config['google_addressbook_client_redirect_url'] = 'https://mail.aaronsoft.de/?_task=settings&_action=plugin.google_addressbook.auth';
--- apps/roundcube/config/get-orga-niced_config.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-$config['product_name'] = 'get Orga-niced Webmail';
-$config['google_addressbook_client_redirect_url'] = 'https://mail.get-orga-niced.de/?_task=settings&_action=plugin.google_addressbook.auth';
